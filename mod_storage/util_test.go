14264a053308ee6352c0b27db0e455bef9d14108
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-01 14:12:07 +0200

    cleanup

--- mod_storage/util_test.go
+++ mod_storage/util_test.go
@@ -1,30 +1,11 @@
 package mod_storage_test
 
 import (
-	"database/sql"
-	"fmt"
 	"io"
 	"os"
-	"testing"
 
 	_ "github.com/lib/pq"
 )
-
-func getPostgresDB(t *testing.T) (*sql.DB, error) {
-	if os.Getenv("PGHOST") == "" {
-		t.SkipNow()
-	}
-
-	connStr := fmt.Sprintf(
-		"user=%s password=%s port=%s host=%s dbname=%s sslmode=disable",
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-		os.Getenv("PGPORT"),
-		os.Getenv("PGHOST"),
-		os.Getenv("PGDATABASE"))
-
-	return sql.Open("postgres", connStr)
-}
 
 func copyFileContents(src, dst string) (err error) {
 	in, err := os.Open(src)
